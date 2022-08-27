2ac857205cb03f29f5a0f6019405c9b0b9c54b57
Mike Koller <mkoller@eml.cc>
2022-08-27 10:37:24 -0600

    Add additional handlers

--- main.go
+++ main.go
@@ -10,10 +10,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from OnlyDupes"))
 }
 
+func dupeView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific dupe..."))
+}
+
+func dupeCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Add a new dupe..."))
+}
+
 func main() {
-	// Init router, then register the home handler
+	// Init router, then register handler functions
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/dupe/view", dupeView)
+	mux.HandleFunc("/dupe/create", dupeCreate)
 
 	// Init web server
 	log.Print("Starting server on :4000")
