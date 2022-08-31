405ea6c2f86f236043555fdd98f9c7a2b4f1759b
Mike Koller <mkoller@eml.cc>
2022-08-31 10:19:03 -0600

    Add custom logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,21 +3,30 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/dupe/view", dupeView)
 	mux.HandleFunc("/dupe/create", dupeCreate)
 
+	srv := &http.Server{
+		Addr:     ":4000",
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
 	// Init web server
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	infoLog.Print("Starting server on :4000")
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
