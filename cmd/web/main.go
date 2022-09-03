4b92d04042588e0de633540e73d80006b2b6bff6
Mike Koller <mkoller@eml.cc>
2022-09-03 10:25:53 -0600

    Connect and test the database connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -12,8 +16,20 @@ type application struct {
 }
 
 func main() {
+	// REMOVE PASS
+	dsn := flag.String("dsn", "web:pass@/onlydupes?parseTime=true", "MySQL data source name")
+
+	flag.Parse()
+
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -28,6 +44,17 @@ func main() {
 
 	// Init web server
 	infoLog.Print("Starting server on :4000")
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
