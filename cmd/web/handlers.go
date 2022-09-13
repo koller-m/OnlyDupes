6b6b4a1550e113995d4736454bf3ab1586e35566
Mike Koller <mkoller@eml.cc>
2022-09-13 16:03:08 -0600

    Create template to submit new dupes

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -50,7 +50,9 @@ func (app *application) dupeView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) dupeCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display the form for creating a new dupe..."))
+	data := app.newTemplateData(r)
+
+	app.render(w, http.StatusOK, "create.tmpl.html", data)
 }
 
 func (app *application) dupeCreatePost(w http.ResponseWriter, r *http.Request) {
