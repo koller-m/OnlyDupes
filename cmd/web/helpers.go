8744e49f11d5d796f81bd2edf0c6a6c22302eaed
Mike Koller <mkoller@eml.cc>
2022-09-18 10:21:42 -0600

    Create helper to deal with InvalidDecodeError

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"net/http"
 	"runtime/debug"
 	"time"
+
+	"github.com/go-playground/form/v4"
 )
 
 // Writes error message and stack trace to errorLog
@@ -48,4 +51,23 @@ func (app *application) newTemplateData(r *http.Request) *templateData {
 	return &templateData{
 		CurrentYear: time.Now().Year(),
 	}
+}
+
+// dst is where to decode the form data to
+func (app *application) decodePostForm(r *http.Request, dst any) error {
+	err := r.ParseForm()
+	if err != nil {
+		return err
+	}
+
+	err = app.formDecoder.Decode(dst, r.PostForm)
+	if err != nil {
+		var invalidDecodeError *form.InvalidDecoderError
+
+		if errors.As(err, &invalidDecodeError) {
+			panic(err)
+		}
+		return err
+	}
+	return nil
 }
